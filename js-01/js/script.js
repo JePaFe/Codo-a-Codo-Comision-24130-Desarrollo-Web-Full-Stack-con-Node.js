b0b842b0ae95b3ae6faac9e98def8460f06708f3
Jean Paul Ferreira <jeanpaul@jepafe.net>
2024-04-16 08:32:01 -0300

    JS 01

---
+++ js-01/js/script.js
@@ -0,0 +1,93 @@
+// console.log("Hola desde el .js");
+
+// Variables
+
+// let num;
+// num = 4;
+// console.log(num);
+
+// num = 7;
+// console.log(num);
+
+// num = `D"o"'s'`;
+// console.log(num);
+
+// num = 2 * (4 + 6);
+// console.log(num);
+
+// let numero = 55;
+
+// let nombreCompleto = "Juan Perez";
+
+// Constantes
+
+// const PI = 3.1416;
+// const IVA = 10.5;
+
+// console.log(IVA);
+
+// Tipo de dato
+
+// let dato;
+// // console.log(dato + 3);
+// console.log(dato, typeof dato);
+
+// dato = 5.3434;
+// console.log(dato, dato.toFixed(2), typeof dato);
+
+// dato = "Dos";
+// console.log(dato, typeof dato);
+
+// dato = "10";
+// console.log(dato, typeof dato);
+
+// dato = true;
+// console.log(dato, typeof dato);
+
+// dato = null;
+// console.log(dato, typeof dato);
+
+// dato = undefined;
+// console.log(dato, typeof dato);
+
+// dato = new Number(4.78545);
+// console.log(dato, dato.toFixed(2), typeof dato);
+
+// Parse
+
+// let num = "34.56d7";
+
+// let numeroEntero = parseInt(num);
+// console.log(numeroEntero);
+
+// let numeroDecimal = parseFloat(num);
+// console.log(numeroDecimal);
+
+// let numero = Number(num);
+// console.log(numero);
+
+// Operadores
+
+// let num1 = parseInt("12");
+// let num2 = parseInt("3");
+
+// let resultado = num1 + num2;
+
+// console.log(resultado);
+
+// // resultado = resultado + 3;
+// resultado += 3;
+
+// console.log(resultado);
+
+// prompt
+
+// let numero = prompt("Ingrese un numero: ");
+// numero = parseInt(numero);
+
+let numero = parseInt(prompt("Ingrese un numero: "));
+
+console.log(numero, typeof numero);
+console.log(numero + 12);
+
+document.write(numero);
